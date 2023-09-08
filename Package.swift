87431bcbf7377859a3482b5ecfb567a951cba0e7
Luca Tagliabue <lu.tagliabue@reply.it>
2023-09-08 16:14:04 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "RadioButton",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v14),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "RadioButton",
+            targets: ["RadioButton"]),
+    ],
+    targets: [
+        .target(
+            name: "RadioButton"),
+        .testTarget(
+            name: "RadioButtonTests",
+            dependencies: ["RadioButton"]),
+    ]
+)
---
+++ Sources/RadioButton/RadioButton.swift
@@ -0,0 +1,107 @@
+import SwiftUI
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public typealias RadioButtonRepresentable = CaseIterable & Hashable & Identifiable
+
+#if os(macOS) || os(watchOS) || os(tvOS)
+
+@available(macOS 10.15, watchOS 6.0, tvOS 13.0, *)
+public struct RadioButton<S, I, R>: View where S: StringProtocol,
+                                        I: StringProtocol,
+                                     R: RadioButtonRepresentable,
+                                    R.AllCases: RandomAccessCollection  {
+    
+    public let title: S
+    public let itemTitle: KeyPath<R, I>
+  
+    @Binding var isSelected: R
+    
+    public var body: some View {
+        Picker(title, selection: $isSelected) {
+            ForEach(R.allCases) {
+                Text($0[keyPath: itemTitle]).tag($0)
+            }
+        }
+        .pickerStyle(.radioGroup)
+    }
+}
+#elseif os(iOS)
+@available(iOS 14.0, *)
+public struct RadioButton<S, I, R>: View where S: StringProtocol,
+                                        I: StringProtocol,
+                                     R: RadioButtonRepresentable,
+                                    R.AllCases: RandomAccessCollection  {
+    
+    public let title: S
+    public let itemTitle: KeyPath<R, I>
+  
+    @Binding var isSelected: R
+    
+    public var body: some View {
+        HStack(alignment: .top) {
+            Text(title)
+            
+            VStack(alignment: .leading) {
+                ForEach(R.allCases) {
+                    Item(item: $0,
+                         title: itemTitle,
+                         checked: $isSelected)
+                    .tag($0)
+                }
+            }
+           
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+extension RadioButton {
+    struct Item<S, R>: View where S: StringProtocol, R: Hashable {
+        
+        @State private var isChecked = false
+        
+        let item: R
+        let title: KeyPath<R, S>
+        
+        @Binding var checked: R
+        
+        var body: some View {
+            HStack {
+                if isChecked {
+                    ZStack{
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 20, height: 20)
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 8, height: 8)
+                    }
+                } else {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 20, height: 20)
+                        .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                        .onTapGesture {
+                            isChecked = true
+                        }
+                }
+                
+                Text(item[keyPath: title])
+            }
+            .onAppear {
+                isChecked = checked == item
+            }
+            .onChange(of: isChecked) { newValue in
+                if newValue {
+                    checked = item
+                }
+            }
+            .onChange(of: checked) { newValue in
+                if checked != item {
+                    isChecked = false
+                }
+            }
+        }
+    }
+}
+#endif
