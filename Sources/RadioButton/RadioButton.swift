2b7211e1f0e2e2796b4485ee6cd094b5b5f69a94
Luca Tagliabue <lu.tagliabue@reply.it>
2023-09-11 16:39:09 +0200

    Use button instead of onTapGesture and decrease min supported version for tvOS

--- Sources/RadioButton/RadioButton.swift
+++ Sources/RadioButton/RadioButton.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 16.0, *)
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 14.0, *)
 public typealias RadioButtonRepresentable = CaseIterable & Hashable & Identifiable
 
-@available(macOS 10.15, iOS 14.0, watchOS 6.0, tvOS 16.0, *)
+@available(macOS 10.15, iOS 14.0, watchOS 6.0, tvOS 14.0, *)
 public struct RadioButton<S, I, R>: View where S: StringProtocol,
                                         I: StringProtocol,
                                      R: RadioButtonRepresentable,
@@ -50,7 +50,7 @@ struct ContentView<S, I, R>: View where S: StringProtocol,
     }
 }
 #elseif os(iOS) || os(tvOS)
-@available(iOS 14.0, tvOS 16.0, *)
+@available(iOS 14.0, tvOS 14.0, *)
 struct ContentView<S, I, R>: View where S: StringProtocol,
                                         I: StringProtocol,
                                      R: RadioButtonRepresentable,
@@ -78,7 +78,7 @@ struct ContentView<S, I, R>: View where S: StringProtocol,
     }
 }
 
-@available(iOS 14.0, tvOS 16.0, *)
+@available(iOS 14.0, tvOS 14.0, *)
 extension ContentView {
     struct Item<S, R>: View where S: StringProtocol, R: Hashable {
         
@@ -101,13 +101,14 @@ extension ContentView {
                             .frame(width: 8, height: 8)
                     }
                 } else {
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: 20, height: 20)
-                        .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                        .onTapGesture {
-                            isChecked = true
-                        }
+                    Button {
+                        isChecked = true
+                    } label: {
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 20, height: 20)
+                            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    }
                 }
                 
                 Text(item[keyPath: title])
