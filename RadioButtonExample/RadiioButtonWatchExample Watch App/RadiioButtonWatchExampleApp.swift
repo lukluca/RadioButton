fc9950c177a2726abc43ebce0120698d5be87bf7
Luca Tagliabue <lu.tagliabue@reply.it>
2023-09-14 18:55:10 +0200

    Add test apps

---
+++ RadioButtonExample/RadiioButtonWatchExample Watch App/RadiioButtonWatchExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiioButtonWatchExampleApp.swift
+//  RadiioButtonWatchExample Watch App
+//
+//  Created by softwave on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RadiioButtonWatchExample_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
